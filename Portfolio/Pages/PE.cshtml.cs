a86487c39a75b686f17078a8857418f6fee76479
Caden Thompson <Cadenjacobthompson2@gmail.com>
2025-09-17 15:22:55 -0500

    Inital Push

---
+++ Portfolio/Pages/PE.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class PEModel : PageModel
+    {
+        private readonly ILogger<PEModel> _logger;
+
+        public PEModel(ILogger<PEModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
